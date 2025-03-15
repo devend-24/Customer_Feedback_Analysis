5a290030945409da9b21fbef2a3df84a9cd0a79f
Devendra <148638044+devend-24@users.noreply.github.com>
2025-03-15 22:51:13 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from nltk.sentiment import SentimentIntensityAnalyzer
 
 class Product(models.Model):
     p_id = models.AutoField(primary_key=True)
@@ -13,13 +14,27 @@ class Feedback(models.Model):
     p_id = models.ForeignKey(Product, on_delete=models.CASCADE)  # FK with delete cascade
     rating = models.IntegerField()
     review_text = models.TextField()
-    
+
+    # NO need of manually adding sentiment
     SENTIMENT_CHOICES = [
         ('positive', 'Positive'),
         ('neutral', 'Neutral'),
         ('negative', 'Negative'),
     ]
     sentiment = models.CharField(max_length=10, choices=SENTIMENT_CHOICES, default='neutral')
+    
+    def save(self, *args, **kwargs):
+        #automatially classify sentiment before saving
+        sia = SentimentIntensityAnalyzer()
+        score = sia.polarity_scores(self.review_text)['compound']
+        if score >= 0.05:
+            self.sentiment = "positive"
+        elif score <= -0.05:
+            self.sentiment = "negative"
+        else:
+            self.sentiment = "neutral"
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Review {self.f_id} for {self.p_id.name} ({self.sentiment})"
